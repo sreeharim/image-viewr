a3207a3e2bec1fa719111c2908db24533e8143ec
sreeharim <sreeharim1992@gmail.com>
2019-06-29 23:43:54 +0530

    Adding db

--- src/App.js
+++ src/App.js
@@ -32,22 +32,80 @@ class App extends React.Component {
     );
   }
   likePlus = index => {
+    let url="http://localhost:4000/votes/updatelike/";
+    let id ="";
+    let likes = 0;
     const images = this.state.images.map((e, i) => {
       if (i == index) {
         e.likes++;
+        id=e._id;
+        likes=e.likes;
       }
       return e;
     });
     this.setState({ images });
+    url+= id;
+    const body ={_id:id,likes}
+    window
+    .fetch(url, {
+      method: "post",
+      headers: {
+              'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(body)
+    })
+    .then(response => {
+      response
+        .text()
+        .then(data => {
+
+        })
+        .catch(e => {
+          console.error(e);
+        });
+    })
+    .catch(e => {
+      console.error(e);
+    });
+
   };
   dislikePlus = index => {
+    let url="http://localhost:4000/votes/updatedislike/";
+    let id ="";
+    let dislikes= 0;
     const images = this.state.images.map((e, i) => {
       if (i == index) {
         e.dislikes++;
+        id=e._id;
+        dislikes=e.dislikes;
       }
       return e;
     });
     this.setState({ images });
+    url+= id;
+    const body ={_id:id,dislikes}
+    window
+    .fetch(url, {
+      method: "post",
+      headers: {
+              'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(body)
+    })
+    .then(response => {
+      response
+        .text()
+        .then(data => {
+
+        })
+        .catch(e => {
+          console.error(e);
+        });
+    })
+    .catch(e => {
+      console.error(e);
+    });
+
   };
 
   sortByLikes = () => {
@@ -82,9 +140,9 @@ class App extends React.Component {
         response
           .text()
           .then(data => {
-            this.setState({
-              images: this.parsePhotosData(data)
-            });
+            //this.setState({images: this.parsePhotosData(data)  });
+              this.addtoDb(this.parsePhotosData(data) );
+
           })
           .catch(e => {
             console.error(e);
@@ -94,7 +152,63 @@ class App extends React.Component {
         console.error(e);
       });
   };
+  addtoDb = (data)=>{
+    const url= "http://localhost:4000/votes/add";
+    window
+      .fetch(url, {
+        method: "post",
+        headers: {
+          Accept: "application/text",
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => {
+        response
+          .text()
+          .then(data => {
+            //this.setState({images: this.parsePhotosData(data)  });
+              this.getPhotos();
 
+          })
+          .catch(e => {
+            console.error(e);
+            this.getPhotos();
+          });
+      })
+      .catch(e => {
+        console.error(e);
+        this.getPhotos();
+      });
+  }
+
+  getPhotos = ()=>{
+    const url= "http://localhost:4000/votes/";
+    window
+      .fetch(url, {
+        method: "get",
+        headers: {
+          Accept: "application/json"
+        }
+      })
+      .then(response => {
+        response
+          .text()
+          .then(data => {
+            this.setState({images: JSON.parse(data)  });
+            
+
+          })
+          .catch(e => {
+            console.error(e);
+            
+          });
+      })
+      .catch(e => {
+        console.error(e);
+        
+      });
+  }
   parsePhotosData = data => {
     const parsedData = new window.DOMParser().parseFromString(data, "text/xml");
     const childNodes = parsedData.documentElement.childNodes[1].childNodes;
@@ -108,7 +222,7 @@ class App extends React.Component {
       const title = this.getNodeAttribute(node.attributes, "title");
 
       return {
-        id: id,
+        _id: id,
         title: title,
         source: this.formatUrl(farm, server, id, secret),
         likes:0,
